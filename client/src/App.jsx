ab74d87df994b96a5174f7b8a77aa61630fa9ac0
BHUVANESWARI BABU <info.bhuvaneswari@gmail.com>
2026-02-19 22:59:48 +0000

    importing dashboard

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router";
 import Signup from "./pages/Signup";
-// import Dashboard from "./pages/Dashboard";
+import Dashboard from "./pages/Dashboard";
 import Login from "./pages/Login";
 
 import { useEffect } from "react";
